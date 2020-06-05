73adbc252f4457cba0af0023ff5a0c430ad16833
Damian Szczurowski <damian.szczurowski@jcommerce.pl>
2020-06-05 12:10:59 +0200

    Updates

---
+++ precache-manifest.49cb271335fd3e056c3a8d7c42d0157a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f89136f3af42c3da202fd06edd0a092",
+    "url": "/checkout-page/index.html"
+  },
+  {
+    "revision": "e1bd4a13334d254ee1c8",
+    "url": "/checkout-page/static/css/main.7722a0e3.chunk.css"
+  },
+  {
+    "revision": "be780e499d8fc3ee9543",
+    "url": "/checkout-page/static/js/2.ae659504.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkout-page/static/js/2.ae659504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1bd4a13334d254ee1c8",
+    "url": "/checkout-page/static/js/main.dc90a973.chunk.js"
+  },
+  {
+    "revision": "f057156125910c4626a1",
+    "url": "/checkout-page/static/js/runtime-main.13596de9.js"
+  },
+  {
+    "revision": "5d2b8b6a1b9e75c10bdb4920b57b737d",
+    "url": "/checkout-page/static/media/product.5d2b8b6a.png"
+  }
+]);
